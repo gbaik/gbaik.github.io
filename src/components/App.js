dd0a76f9438205b2897145320c1634f0a23a5b9c
Gideon Baik <gideonbaik@gmail.com>
2018-03-20 00:14:19 -0700

    Modify setup

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div >
+    <div>
+      Hello World
+    </div>
+    <div>
+      I'm Gideon
+    </div>
+    LinkedIn
+    Github
+    Email
+  </div>
+)
+
+// #695a88
+// #44316b
+export default App;
